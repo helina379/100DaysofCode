affc77b5dfaa4d59efe2b0f7716c36acf4be9130
helina379 <krishnavivek1234@gmail.com>
2026-07-06 23:31:29 +0530

    Update main.py

--- day14_higher_or_lower/main.py
+++ day14_higher_or_lower/main.py
@@ -4,12 +4,12 @@ import random
 print(art.logo)
 
 
-
 score = 0
 def game():
     global score
     option1 = data[random.randint(0, len(data) - 1)]
     option2 = data[random.randint(0, len(data) - 1)]
+    option3 = data[random.randint(0, len(data) - 1)]
     if option1 != option2:
         print("Compare A: " + option1['name'] + ', ' + option1['description'] + ', ' + "from " +option1['country'])
         print(art.vs)
@@ -20,14 +20,55 @@ def game():
             if option1['follower_count'] > option2['follower_count']:
                 score += 1
                 print(f"You're right! Score: {score}")
-                game()
+                print("Compare A: " + option2['name'] + ', ' + option2['description'] + ', ' + "from " + option2[
+                    'country'])
+                print(art.vs)
+                # print("Against B: " + data[random.randint(0, len(data)-1)])
+                print("Against B: " + option3['name'] + ', ' + option3['description'] + ', ' + "from " + option3[
+                    'country'])
+                answer = input("Who has more followers 'A' or 'B': ").lower()
+                if answer == 'a':
+                    if option2['follower_count'] > option3['follower_count']:
+                        score += 1
+                        print(f"You're right! Score: {score}")
+                        game()
+                    else:
+                        print(f"HAHA YOU LOSE! {score}")
+                if answer == 'b':
+                    if option3['follower_count'] > option2['follower_count']:
+                        score += 1
+                        print(f"You're right! Score: {score}")
+                        game()
+                    else:
+                        print(f"HAHA YOU LOSE! {score}")
             else:
                 print(f"HAHA YOU LOSE! {score}")
         elif answer == 'b':
             if option2['follower_count'] > option1['follower_count']:
                 score += 1
                 print(f"You're right! Score: {score}")
-                game()
+                print("Compare A: " + option2['name'] + ', ' + option2['description'] + ', ' + "from " + option2[
+                    'country'])
+                print(art.vs)
+                # print("Against B: " + data[random.randint(0, len(data)-1)])
+                print("Against B: " + option3['name'] + ', ' + option3['description'] + ', ' + "from " + option3[
+                    'country'])
+                answer = input("Who has more followers 'A' or 'B': ").lower()
+                if answer == 'a':
+                    if option2['follower_count'] > option3['follower_count']:
+                        score += 1
+                        print(f"You're right! Score: {score}")
+                        game()
+                    else:
+                        print(f"HAHA YOU LOSE! {score}")
+                if answer == 'b':
+                    if option3['follower_count'] > option2['follower_count']:
+                        score += 1
+                        print(f"You're right! Score: {score}")
+                        game()
+                    else:
+                        print(f"HAHA YOU LOSE! {score}")
+
             else:
                 print(f"HAHA YOU LOSE! {score}")
 
